5c4db63056b6068bbc54beb9a0b2886aee5c2902
Rajan Kumar <rajnkumar.1397@gmail.com>
2022-04-15 11:20:03 +0530

    first commit

---
+++ add.js
@@ -0,0 +1,8 @@
+function add(a, b) {
+  console.log(a + b);
+}
+function power(a, b) {
+  console.log(a ** b);
+}
+
+module.exports = { add, power };
---
+++ main.js
@@ -0,0 +1,16 @@
+// indecAdd is itself a function
+
+const { add, power } = require("./add");
+
+const indexSub = require("./subtract");
+
+const indexMul = require("./multiply");
+
+const indexDiv = require("./divide");
+
+add(15, 5);
+indexSub(15, 5);
+indexMul(15, 5);
+indexDiv(15, 5);
+
+power(2, 3);
